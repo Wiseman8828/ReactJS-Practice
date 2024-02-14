a5133af163faf63112cef4ea316fd9cc160050f5
Ashwini kumar <ashwini.kr.260@gmail.com>
2024-02-14 08:41:33 +0530

    basics of react library

---
+++ aks-react/app.js
@@ -0,0 +1,11 @@
+const title = React.createElement('h1',{
+    id:"heading"
+},'Welcome to React!')
+
+const nestedChild = React.createElement('div',{},
+    [
+        React.createElement('h2',{},'hey i\'m 1st sibling'),
+        React.createElement('h2',{}, 'hey i\'m 1st sibling')
+    ])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(nestedChild)
